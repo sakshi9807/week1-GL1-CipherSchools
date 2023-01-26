c7abf5b684977f8088f7e229f8b7ae22ae0d7b3c
sakshi9807 <sakshigirdhar041@gmail.com>
2023-01-26 16:14:21 +0530

    question

---
+++ Rotational.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+var alphabet=map [int]string{
+	1:"a",
+	2:"b",
+	3:"c",
+	4:"d",
+	5:"e",
+	6:"f",
+	7:"g",
+	8:"h",
+	9:"i",
+	10:"j",
+	11:"k",
+	12:"l",
+	13:"m",
+	14:"n",
+	15:"o",
+	16:"p",
+	17:"q",
+	18:"r",
+	19:"s",
+	20:"t",
+	21:"u",
+	22:"v",
+	23:"w",
+	24:"x",
+	25:"y",
+	26:"z",
+}
+func GetKey(letter string)key int {
+	for _,value:=range alphabet{
+		if value == letter{
+			key = index
+			break
+		}
+	}
+	return
+}
+
+func RotationalCipher(original string) encryptic string{
+	for index,value:=range original{
+shiftKey := GetKey(string (value))+5
+if shiftKey>26{
+	shiftKey=shiftKey%26
+}
+encryptic+= alphabet[shiftKey]
+	}
+	return
+}
+func main() {
+	fmt.Println(RotationalCipher("omg"))
+}
